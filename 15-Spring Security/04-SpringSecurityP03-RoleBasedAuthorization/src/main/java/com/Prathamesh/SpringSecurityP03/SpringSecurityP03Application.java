a7fc01db46d9bf829b8f31f37c07a69542cba5e7
Prathamesh <prathameshaj2004@gmail.com>
2025-09-06 23:21:27 +0530

    Java Full Satck

---
+++ 15-Spring Security/04-SpringSecurityP03-RoleBasedAuthorization/src/main/java/com/Prathamesh/SpringSecurityP03/SpringSecurityP03Application.java
@@ -0,0 +1,14 @@
+package com.Prathamesh.SpringSecurityP03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityP03Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringSecurityP03Application.class, args);
+	}
+
+}
