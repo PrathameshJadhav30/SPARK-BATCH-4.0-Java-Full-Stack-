69353d6fb4491db9cd1f2c44edfd5891e5ec38b9
Prathamesh <prathameshaj2004@gmail.com>
2025-09-08 00:39:59 +0530

    Java Full Stack

---
+++ 15-Spring Security/05-SpringSecurityP04/src/main/java/com/Prathamesh/SpringSecurityP04/SpringSecurityP04Application.java
@@ -0,0 +1,14 @@
+package com.Prathamesh.SpringSecurityP04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityP04Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringSecurityP04Application.class, args);
+	}
+
+}
