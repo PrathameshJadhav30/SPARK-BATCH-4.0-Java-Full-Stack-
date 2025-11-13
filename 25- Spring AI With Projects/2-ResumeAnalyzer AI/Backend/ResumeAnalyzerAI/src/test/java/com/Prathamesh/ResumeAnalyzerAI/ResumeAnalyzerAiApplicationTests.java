511f1e37862a6ccc513a7c241c81be81737286bb
Prathamesh <prathameshaj2004@gmail.com>
2025-11-13 23:09:41 +0530

    Java Full Stack

---
+++ 25- Spring AI With Projects/2-ResumeAnalyzer AI/Backend/ResumeAnalyzerAI/src/test/java/com/Prathamesh/ResumeAnalyzerAI/ResumeAnalyzerAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Prathamesh.ResumeAnalyzerAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeAnalyzerAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
